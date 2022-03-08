55df518ab042a52e8418229f947cf759573845bc
RodrigoFdz01 <rodrigof3012@gmail.com>
2022-03-08 16:02:53 -0300

    rama de codificandolo

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,25 +1,14 @@
-import React, { useEffect, useState } from "react";
-import { todosPersonajes } from "../Functions/Functions";
+//import { todosPersonajes } from "../Functions/Functions";
 import style from "../style/Card.module.css";
-//import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-const Card = () => {
-  const [personajes, setPersonajes] = useState(null);
-  useEffect(() => {
-    todosPersonajes(setPersonajes);
-  }, []);
+const Card = ({ todosPersonajes }) => {
   return (
     <div className={style.card}>
-      {personajes != null
-        ? personajes.map((personaje) => (
-            <div key={personaje.id}>
-              <img className={style.foto} src={personaje.image} alt="" />
-              <a className={style.name} href={`personaje/${personaje.id}`}>
-                {personaje.name}{" "}
-              </a>
-            </div>
-          ))
-        : "no hay lista de personajes"}
+      <img className={style.foto} src={todosPersonajes.image} alt="" />
+      <Link to={`/personaje/${todosPersonajes.id}`}>
+        <p className={style.name}>{todosPersonajes.name}</p>
+      </Link>
     </div>
   );
 };
--- src/Components/Inicio.jsx
+++ src/Components/Inicio.jsx
@@ -1,26 +1,30 @@
 import React, { useEffect, useState } from "react";
-import { todosPersonajes } from "../Functions/Functions";
+import { todosPersonajes } from "../Functions/Functions.js";
 import style from "../style/Inicio.module.css";
-import Card from "./Card";
+import { Link } from "react-router-dom";
 
 const Inicio = () => {
   const [personajes, setPersonajes] = useState(null);
+
   useEffect(() => {
     todosPersonajes(setPersonajes);
   }, []);
   return (
-    <div className={style.grid}>
-      <Card />
-    </div>
+    <>
+      <div className={style.grid}>
+        {personajes != null
+          ? personajes.map((personaje) => (
+              <Link to={`/personaje/${personaje.id}`}>
+                <div className={style.card} key={personaje.id}>
+                  <img className={style.picture} src={personaje.image} alt="" />
+                  <p className={style.name}>{personaje.name}</p>
+                </div>
+              </Link>
+            ))
+          : "no hay info"}
+      </div>
+    </>
   );
 };
 
 export default Inicio;
-
-/*
-<div className={styles.gridcard}>
-     <div className={styles.gridcard}>
-      <Card />
-    </div>
-    </div>
-*/
