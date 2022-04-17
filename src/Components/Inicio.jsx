ce71d211d10db4c57157eb20c85a85295de097de
RodrigoFdz01 <rodrigof3012@gmail.com>
2022-04-17 20:29:12 -0300

    hac el filtro

--- src/Components/Inicio.jsx
+++ src/Components/Inicio.jsx
@@ -1,29 +1,58 @@
 import React, { useState, useEffect } from "react";
 import { todosPersonajes } from "../Functions/Functions";
 import style from "../style/Inicio.module.css";
-import Filtro from "./Filtros";
+import imagen from "../rickandmorty.png";
 
 const Inicio = () => {
   const [personajes, setPersonajes] = useState(null);
+  const [search, setSearch] = useState("");
+  //console.log(search);
+
+  function onClick() {
+    setSearch("");
+  }
+  function handleChange(event) {
+    setSearch(event.target.value);
+    console.log(search);
+  }
+  /*personajes.filter((search) =>
+        search.name.toLowercase().includes(search.toLowercase())
+        */
 
   useEffect(() => {
     todosPersonajes(setPersonajes);
   }, []);
 
   return (
-    <div className={style.card}>
-      <Filtro />
-      {personajes != null
-        ? personajes.map((personaje) => (
-            <div key={personaje.id}>
-              <img className={style.foto} src={personaje.image} alt="" />
-              <a className={style.name} href={`personaje/${personaje.id}`}>
-                {personaje.name}{" "}
-              </a>
-            </div>
-          ))
-        : "no hay lista de personajes"}
-    </div>
+    <>
+      <div className={style.picture}>
+        <img src={imagen} className={style.image} alt="imagen" />
+      </div>
+      <div className={style.navfilter}>
+        <input
+          className={style.inputText}
+          type="text"
+          placeholder="Write your character"
+          value={search}
+          onChange={handleChange}
+        />
+        <button onClick={onClick} type="submit">
+          Clear
+        </button>
+      </div>
+      <div className={style.card}>
+        {personajes != null
+          ? personajes.map((personaje) => (
+              <div key={personaje.id}>
+                <img className={style.foto} src={personaje.image} alt="" />
+                <a className={style.name} href={`personaje/${personaje.id}`}>
+                  {personaje.name}{" "}
+                </a>
+              </div>
+            ))
+          : "ddddd"}
+      </div>
+    </>
   );
 };
 
